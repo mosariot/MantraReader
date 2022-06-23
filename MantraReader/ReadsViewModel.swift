523dbd73ee43579dbf4ebfb8c189627c8a0e73cc
mosariot <56510738+mosariot@users.noreply.github.com>
2022-06-23 10:50:51 +0300

    Animation for displayedGoal number implementation

--- MantraReader/ReadsViewModel.swift
+++ MantraReader/ReadsViewModel.swift
@@ -11,18 +11,22 @@ import Combine
 final class ReadsViewModel: ObservableObject {
     @Published var mantra: Mantra
     @Published var displayedReads: Double
-    @Published var displayedGoal: Int32
+    @Published var displayedGoal: Double
     @Published var progress: Double
     @Published var isAnimated: Bool = false
+
     private var deltaReads: Double = 0
-    private var elapsedTime: Double = 0
-    private var timer = Timer.publish(every: Constants.animationTime / 100, on: .main, in: .common)
-    private var timerSubscription: Cancellable?
+    private var elapsedTimeReads: Double = 0
+    private var timerReadsSubscription: Cancellable?
+    private var deltaGoal: Double = 0
+    private var elapsedTimeGoal: Double = 0
+    private var timerGoalSubscription: Cancellable?
+
     
     init(_ mantra: Mantra) {
         self.mantra = mantra
         self.displayedReads = Double(mantra.reads)
-        self.displayedGoal = mantra.goal
+        self.displayedGoal = Double(mantra.goal)
         self.progress = Double(mantra.reads) / Double(mantra.goal)
     }
     
@@ -31,17 +35,17 @@ final class ReadsViewModel: ObservableObject {
         mantra.reads = Int32(value) ?? mantra.reads
         progress = Double(mantra.reads) / Double(mantra.goal)
         deltaReads = Double(mantra.reads) - displayedReads
-        timerSubscription = Timer.publish(every: Constants.animationTime / 100, on: .main, in: .common)
+        timerReadsSubscription = Timer.publish(every: Constants.animationTime / 100, on: .main, in: .common)
             .autoconnect()
             .sink { _ in
-                if self.elapsedTime < Constants.animationTime {
+                if self.elapsedTimeReads < Constants.animationTime {
                     self.displayedReads += Double(self.deltaReads) / 100.0
-                    self.elapsedTime += Constants.animationTime / 100
+                    self.elapsedTimeReads += Constants.animationTime / 100
                 } else {
                     self.displayedReads = Double(self.mantra.reads)
-                    self.elapsedTime = 0
+                    self.elapsedTimeReads = 0
                     self.isAnimated = false
-                    self.timerSubscription?.cancel()
+                    self.timerReadsSubscription?.cancel()
                 }
             }
     }
@@ -49,11 +53,21 @@ final class ReadsViewModel: ObservableObject {
     func animateGoalsChanges(with value: String) {
         isAnimated = true
         mantra.goal = Int32(value) ?? mantra.goal
-        displayedGoal = mantra.goal
         progress = Double(mantra.reads) / Double(mantra.goal)
-        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.animationTime) {
-            self.isAnimated = false
-        }
+        deltaGoal = Double(mantra.goal) - displayedGoal
+        timerGoalSubscription = Timer.publish(every: Constants.animationTime / 100, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                if self.elapsedTimeGoal < Constants.animationTime {
+                    self.displayedGoal += Double(self.deltaGoal) / 100.0
+                    self.elapsedTimeGoal += Constants.animationTime / 100
+                } else {
+                    self.displayedGoal = Double(self.mantra.goal)
+                    self.elapsedTimeGoal = 0
+                    self.isAnimated = false
+                    self.timerGoalSubscription?.cancel()
+                }
+            }
     }
 }
 

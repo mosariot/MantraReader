27c1dd968cc9b9c7df8e6b34e25c0812bca3e433
mosariot <56510738+mosariot@users.noreply.github.com>
2022-06-24 08:30:00 +0300

    Container tuning. preloadData method addition.

--- MantraReader/Persistence.swift
+++ MantraReader/Persistence.swift
@@ -14,15 +14,47 @@ struct PersistenceController {
     
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "MantraReader")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
+        if inMemory { container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null") }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
+            guard let error = error as NSError? else { return }
+            fatalError("Failed to load persistent stores: \(error)")
         })
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         container.viewContext.automaticallyMergesChangesFromParent = true
+        do {
+            try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+            fatalError("Failed to pin viewContext to the current generation: \(error)")
+        }
+    }
+    
+    func preloadData() {
+        let context = container.viewContext
+        PreloadedMantras.data.forEach { data in
+            let mantra = Mantra(context: context)
+            mantra.uuid = UUID()
+            data.forEach { key, value in
+                switch key {
+                case .title:
+                    mantra.title = value
+                case .text:
+                    mantra.text = value
+                case .details:
+                    mantra.details = value
+                case .image:
+                    if let image = UIImage(named: value) {
+                        mantra.image = image.pngData()
+                        mantra.imageForTableView = image.resize(to: CGSize(width: Constants.rowHeight, height: Constants.rowHeight)).pngData()
+                    }
+                }
+            }
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
 

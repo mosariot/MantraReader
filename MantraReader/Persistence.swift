c4a28979402db23c04f9814ee5fec536d88a1615
mosariot <56510738+mosariot@users.noreply.github.com>
2022-07-01 09:52:51 +0300

    History tracking implementation

--- MantraReader/Persistence.swift
+++ MantraReader/Persistence.swift
@@ -15,13 +15,25 @@ struct PersistenceController {
     
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "MantraReader")
+        
+         guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("Failed to retrieve a persistent store description.")
+        }
+        
+        // Enable history tracking and remote notifications
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+        description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+        
         if inMemory { container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null") }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             guard let error = error as NSError? else { return }
             fatalError("Failed to load persistent stores: \(error)")
         })
+        
+        // Pin the viewContext to the current generation token and set it to keep itself up to date with local changes
         container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         container.viewContext.automaticallyMergesChangesFromParent = true
+        
         if !inMemory {
             do {
                 try container.viewContext.setQueryGenerationFrom(.current)

c769f32d7baed7e1f3bbe631f3169e44f0a379c3
mosariot <56510738+mosariot@users.noreply.github.com>
2022-07-11 17:27:54 +0300

    UIKit alert implemantation

--- MantraReader/ReadsView.swift
+++ MantraReader/ReadsView.swift
@@ -19,94 +19,88 @@ struct ReadsView: View {
     @ObservedObject var viewModel: ReadsViewModel
     @State private var isPresentedAdjustingAlert = false
     @State private var adjustingType: AdjustingType?
-    @State private var adjustingNumber: Int32?
+    @State private var adjustingText: String = ""
     
 #if os(iOS)
     private var isPhone: Bool { UIDevice.current.userInterfaceIdiom == .phone }
 #endif
     
     var body: some View {
-        VStack {
-            if verticalSizeClass == .compact && isPhone {
-                HStack {
-                    Image(uiImage: viewModel.image)
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .frame(maxHeight: 200)
+        ZStack {
+            VStack {
+                if verticalSizeClass == .compact && isPhone {
+                    HStack {
+                        Image(uiImage: viewModel.image)
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(maxHeight: 200)
                     
-                    CircularProgressView(
-                        progress: viewModel.progress,
-                        displayedNumber: viewModel.displayedReads,
-                        displayedGoal: viewModel.displayedGoal,
-                        isAnimated: viewModel.isAnimated
-                    )
+                        CircularProgressView(
+                            progress: viewModel.progress,
+                            displayedNumber: viewModel.displayedReads,
+                            displayedGoal: viewModel.displayedGoal,
+                            isAnimated: viewModel.isAnimated
+                        )
+                    }
+                } else {
+                    VStack {
+                        Image(uiImage: viewModel.image)
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(maxHeight: 200)
+                    
+                        Text(viewModel.title)
+                            .font(.title)
+                            .padding()
+                    
+                        CircularProgressView(
+                            progress: viewModel.progress,
+                            displayedNumber: viewModel.displayedReads,
+                            displayedGoal: viewModel.displayedGoal,
+                            isAnimated: viewModel.isAnimated
+                        )
+                    }
                 }
-            } else {
-                VStack {
-                    Image(uiImage: viewModel.image)
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .frame(maxHeight: 200)
-                    
-                    Text(viewModel.title)
-                        .font(.title)
-                        .padding()
-                    
-                    CircularProgressView(
-                        progress: viewModel.progress,
-                        displayedNumber: viewModel.displayedReads,
-                        displayedGoal: viewModel.displayedGoal,
-                        isAnimated: viewModel.isAnimated
-                    )
-                }
-            }
             
-            HStack {
-                Button {
-                    adjustingType = .reads
-                    isPresentedAdjustingAlert = true
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: 60))
-                        .symbolVariant(.circle.fill)
+                HStack {
+                    Button {
+                        adjustingType = .reads
+                        isPresentedAdjustingAlert = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.system(size: 60))
+                            .symbolVariant(.circle.fill)
+                    }
+                    .padding()
+                    Button {
+                        adjustingType = .rounds
+                        isPresentedAdjustingAlert = true
+                    } label: {
+                        Image(systemName: "arrow.clockwise")
+                            .font(.system(size: 60))
+                            .symbolVariant(.circle.fill)
+                    }
+                    .padding()
+                    Button {
+                        adjustingType = .value
+                        isPresentedAdjustingAlert = true
+                    } label: {
+                        Image(systemName: "hand.draw")
+                            .font(.system(size: 60))
+                            .symbolVariant(.fill)
+                    }
+                    .padding()
                 }
-                .padding()
-                Button {
-                    adjustingType = .rounds
-                    isPresentedAdjustingAlert = true
-                } label: {
-                    Image(systemName: "arrow.clockwise")
-                        .font(.system(size: 60))
-                        .symbolVariant(.circle.fill)
-                }
-                .padding()
-                Button {
-                    adjustingType = .value
-                    isPresentedAdjustingAlert = true
-                } label: {
-                    Image(systemName: "hand.draw")
-                        .font(.system(size: 60))
-                        .symbolVariant(.fill)
-                }
-                .padding()
             }
-        }
-        .ignoresSafeArea(.keyboard)
-        .alert(
-            viewModel.alertTitle(for: adjustingType),
-            isPresented: $isPresentedAdjustingAlert,
-            presenting: adjustingType
-        ) { adjust in
-            TextField("Enter number", value: $adjustingNumber, format: .number)
-            Button(viewModel.buttonTitle(for: adjust)) {
-                viewModel.handleAdjusting(for: adjust, with: adjustingNumber)
-                adjustingType = nil
-                adjustingNumber = nil
-            }
-//            .disabled(!viewModel.isAllowedAdjusting(for: adjust, with: adjustingNumber))
-            Button("Cancel", role: .cancel) {
-                adjustingType = nil
-                adjustingNumber = nil
+            .ignoresSafeArea(.keyboard)
+            
+            if isPresentedAdjustingAlert {
+                UpdatingAlertView(
+                    isPresented: $isPresentedAdjustingAlert,
+                    adjustingText: $adjustingText,
+                    adjustingType: $adjustingType,
+                    viewModel: viewModel
+                )
             }
         }
     }
@@ -134,5 +128,111 @@ struct ReadsView_Previews: PreviewProvider {
                 viewContext: controller.container.viewContext
             )
         )
+    }
+}
+
+struct UpdatingAlertView: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    @Binding var adjustingText: String
+    @Binding var adjustingType: AdjustingType?
+    var viewModel: ReadsViewModel
+    
+    func makeUIViewController(
+        context: UIViewControllerRepresentableContext<UpdatingAlertView>
+    ) -> UIViewController {
+        UIViewController()
+    }
+    
+    func updateUIViewController(
+        _ uiViewController: UIViewController,
+        context: UIViewControllerRepresentableContext<UpdatingAlertView>
+    ) {
+        guard context.coordinator.alert == nil else { return }
+        
+        if isPresented {
+            var adjustingNumber: Int32 = 0
+            let (alertTitle, actionTitle) = viewModel.alertAndActionTitles(for: adjustingType)
+            
+            let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
+            context.coordinator.alert = alert
+            
+            let positiveAction = UIAlertAction(title: actionTitle, style: .default) { _ in   
+                viewModel.handleAdjusting(for: adjustingType, with: adjustingNumber)
+                alert.dismiss(animated: true) {
+                    adjustingType = nil
+                    adjustingText = ""
+                    isPresented = false
+                }                                                                    
+            }
+            
+            alert.addTextField { alertTextField in
+                alertTextField.placeholder = "Enter number"
+                alertTextField.keyboardType = .numberPad
+                alertTextField.clearButtonMode = .always
+                alertTextField.delegate = context.coordinator 
+                positiveAction.isEnabled = false
+                NotificationCenter.default
+                    .addObserver(
+                        forName: UITextField.textDidChangeNotification,
+                        object: alertTextField,
+                        queue: .main
+                    ) { _ in
+                    if viewModel.isValidUpdatingNumber(
+                        text: alertTextField.text,
+                        adjustingType: adjustingType
+                    ) {
+                        positiveAction.isEnabled = true
+                        guard
+                            let textValue = alertTextField.text,
+                            let numberValue = Int32(textValue)
+                        else { return }
+                        adjustingNumber = numberValue
+                    } else {
+                        positiveAction.isEnabled = false
+                    }
+                }
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in
+                alert.dismiss(animated: true) {
+                    adjustingType = nil
+                    adjustingText = ""
+                    isPresented = false
+                }                                                               
+            }
+            
+            alert.addAction(cancelAction)
+            alert.addAction(positiveAction)
+            alert.view.tintColor = Constants.accentColor ?? .systemOrange
+            
+            DispatchQueue.main.async {
+                uiViewController.present(alert, animated: true, completion: {
+                    adjustingType = nil
+                    adjustingText = ""
+                    isPresented = false
+                    context.coordinator.alert = nil
+                })
+            }
+        }
+    }
+    
+    func makeCoordinator() -> UpdatingAlertView.Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var alert: UIAlertController?
+        var view: UpdatingAlertView
+        
+        init(_ control: AlertControlView) {
+            self.view = view
+        }
+        
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            guard CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) else {
+                return false
+            }
+            return true
+        }
     }
 }

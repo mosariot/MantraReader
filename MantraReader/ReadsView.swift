08d0a82b6d3b42fe448c4186752288e0dd118a76
mosariot <mosariot@gmail.com>
2022-07-13 16:44:59 +0300

    Keyboard avoidance for ReadsView

--- MantraReader/ReadsView.swift
+++ MantraReader/ReadsView.swift
@@ -30,81 +30,87 @@ struct ReadsView: View {
         AnyLayout(_HStackLayout()) :
         AnyLayout(_VStackLayout())
         
+        
         ZStack {
-            VStack {
-                layout {
-                    Spacer()
-                    Image(uiImage: viewModel.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(minHeight: 80, maxHeight: isPhone ? 160 : 260)
-                        .layoutPriority(3)
-                    if verticalSizeClass == .regular {
+            GeometryReader { _ in
+                VStack {
+                    layout {
                         Spacer()
-                        Text(viewModel.title)
-                            .font(.system(.largeTitle, weight: .medium))
-                            .textSelection(.enabled)
-                            .padding()
-                    }
-                    Spacer()
-                    VStack {
-                        CircularProgressView(
-                            progress: viewModel.progress,
-                            displayedNumber: viewModel.displayedReads,
-                            isAnimated: viewModel.isAnimated
-                        )
-                        .padding()
-                        Button("Current goal: \(viewModel.displayedGoal, specifier: "%.0f")") {
-                            adjustingType = .goal
-                            isPresentedAdjustingAlert = true
+                        Image(uiImage: viewModel.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(minHeight: 80, maxHeight: isPhone ? 160 : 260)
+                            .layoutPriority(3)
+                        if verticalSizeClass == .regular {
+                            Spacer()
+                            Text(viewModel.title)
+                                .font(.system(.largeTitle, weight: .medium))
+                                .textSelection(.enabled)
+                                .padding()
                         }
+                        Spacer()
+                        VStack {
+                            CircularProgressView(
+                                progress: viewModel.progress,
+                                displayedNumber: viewModel.displayedReads,
+                                isAnimated: viewModel.isAnimated
+                            )
+                            .padding()
+                            Button("Current goal: \(viewModel.displayedGoal, specifier: "%.0f")") {
+                                adjustingType = .goal
+                                isPresentedAdjustingAlert = true
+                            }
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                    HStack(
+                        spacing: (horizontalSizeClass == .compact
+                                  && !(verticalSizeClass == .compact && isPhone)) ? 10 : 50
+                    ) {
+                        Button {
+                            adjustingType = .reads
+                            isPresentedAdjustingAlert = true
+                        } label: {
+                            Image(systemName: "plus")
+                                .font(.system(size: 60))
+                                .symbolVariant(.circle.fill)
+                        }
+                        .padding()
+                        Button {
+                            adjustingType = .rounds
+                            isPresentedAdjustingAlert = true
+                        } label: {
+                            Image(systemName: "arrow.clockwise")
+                                .font(.system(size: 60))
+                                .symbolVariant(.circle.fill)
+                        }
+                        .padding()
+                        Button {
+                            adjustingType = .value
+                            isPresentedAdjustingAlert = true
+                        } label: {
+                            Image(systemName: "hand.draw")
+                                .font(.system(size: 60))
+                                .symbolVariant(.fill)
+                        }
+                        .padding()
+                    }
                 }
-                HStack(
-                    spacing: (horizontalSizeClass == .compact
-                              && !(verticalSizeClass == .compact && isPhone)) ? 10 : 50
-                ) {
-                    Button {
-                        adjustingType = .reads
-                        isPresentedAdjustingAlert = true
-                    } label: {
-                        Image(systemName: "plus")
-                            .font(.system(size: 60))
-                            .symbolVariant(.circle.fill)
-                    }
-                    .padding()
-                    Button {
-                        adjustingType = .rounds
-                        isPresentedAdjustingAlert = true
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                            .font(.system(size: 60))
-                            .symbolVariant(.circle.fill)
-                    }
-                    .padding()
-                    Button {
-                        adjustingType = .value
-                        isPresentedAdjustingAlert = true
-                    } label: {
-                        Image(systemName: "hand.draw")
-                            .font(.system(size: 60))
-                            .symbolVariant(.fill)
-                    }
-                    .padding()
+                .ignoresSafeArea(.keyboard)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                if isPresentedAdjustingAlert {
+                    UpdatingAlertView(
+                        isPresented: $isPresentedAdjustingAlert,
+                        adjustingType: $adjustingType,
+                        viewModel: viewModel
+                    )
                 }
             }
-            
-            if isPresentedAdjustingAlert {
-                UpdatingAlertView(
-                    isPresented: $isPresentedAdjustingAlert,
-                    adjustingType: $adjustingType,
-                    viewModel: viewModel
-                )
+            .toolbar {
+                
             }
-        }
-        .toolbar {
-            
+            .ignoresSafeArea(.keyboard)
         }
     }
 }

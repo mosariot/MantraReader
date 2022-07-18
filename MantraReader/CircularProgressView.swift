36f7ab120a84e3ce1d393a55dd27c0cbf2f47aab
mosariot <mosariot@gmail.com>
2022-07-18 07:34:02 +0300

    Stroke color depending on progress. Disable dynamicType for reads number

--- MantraReader/CircularProgressView.swift
+++ MantraReader/CircularProgressView.swift
@@ -23,7 +23,7 @@ struct CircularProgressView: View {
                 Circle()
                     .trim(from: 0, to: progress)
                     .stroke(
-                        .pink,
+                        strokeColor(for: progress),
                         style: StrokeStyle(
                             lineWidth: 20,
                             lineCap: .round
@@ -40,14 +40,24 @@ struct CircularProgressView: View {
                     .font(.system(.largeTitle, design: .rounded, weight: .medium))
                     .textSelection(.enabled)
                     .bold()
+                    .dynamicTypeSize(.medium)
             }
+        }
+    }
+    
+    private func strokeColor(for progress: Double) -> Color {
+        switch progress {
+        case 0..<0.5: return .green
+        case 0.5..<1.0: return .orange
+        case 1.0...: return .pink
+        default: return .green
         }
     }
 }
 
 struct CircularProgressView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularProgressView(progress: 0.2, displayedNumber: 250, isAnimated: true)
+        CircularProgressView(progress: 0.4, displayedNumber: 250, isAnimated: true)
             .previewLayout(.fixed(width: 300, height: 300))
             .padding()
             .previewDisplayName("Circular Progress View")

558ca5d62b885dd21cf4a0e0f4fdcdb1b18f1649
mosariot <56510738+mosariot@users.noreply.github.com>
2022-07-29 12:21:46 +0300

    First iteration for PreloadedMantraLisViewModel

--- MantraReader/PreloadedMantraLisViewModel.swift
+++ MantraReader/PreloadedMantraLisViewModel.swift
@@ -5,4 +5,59 @@
 //  Created by Александр Воробьев on 29.07.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct PreloadedMantra: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let image: UIImage?
+    var isSelected: Bool = false
+}
+
+final class PreloadedMantraLisViewModel: ObservabledObject {
+  @Published var mantras: [PreloadedMantra]
+  @Published var selectedMantrasTitles: Set<String>
+  
+  init() {
+    mantras = getPreloadedMantras()
+    
+  }
+  
+  func select(mantra: PreloadedMantra) {
+    if selectedMantrasTitles.contains(mantra.title) {
+        if let index = mantras.firstIndex(where: { $0.title == mantra.title }) {
+            mantras[index].isSelected = false
+        }
+        selectedMantrasTitles.remove(mantra.title)
+    } else {
+        if let index = mantras.firstIndex(where: { $0.title == mantra.title }) {
+            mantras[index].isSelected = true
+        }
+        selectedMantrasTitles.insert(mantra.title)
+    }
+  }
+  
+  func addMantras() {
+  }
+  
+  private func getPreloadedMantras() -> [PreloadedMantra] {
+        var mantras: [PreloadedMantra] = []
+        PreloadedMantras.sortedData().forEach { data in
+            let mantra = PreloadedMantra()
+            data.forEach { key, value in
+                if key == .title {
+                    mantra.title = value
+                }
+                if key == .image {
+                    if let image = UIImage(named: value) {
+                        mantra.image = image.resize(to: CGSize(width: Constants.rowHeight/2, height: Constants.rowHeight/2))
+                    } else {
+                        mantra.image = UIImage(named: Constants.defaultImage)?.resize(to: CGSize(width: Constants.rowHeight/2, height: Constants.rowHeight/2))
+                    }
+                }
+            }
+            mantras.append(mantra)
+        }
+        return mantras
+    }
+}

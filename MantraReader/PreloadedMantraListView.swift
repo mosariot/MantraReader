1b49b9231167eee861994c89896b5c1dd3fc55d3
mosariot <56510738+mosariot@users.noreply.github.com>
2022-07-31 11:40:57 +0300

    Confirmation dialog for duplication in PreloadedMantraListView

--- MantraReader/PreloadedMantraListView.swift
+++ MantraReader/PreloadedMantraListView.swift
@@ -18,6 +18,21 @@ struct PreloadedMantraListView: View {
                     viewModel.select(mantra)
                 }
         }
+        .confirmationDialog(
+            "Duplicating Mantras",
+            isPresented: $viewModel.isDuplicating
+        ) {
+            Button {
+                withAnimation {
+                    viewModel.addMantras()
+                    isPresented = false
+                }
+            } label: {
+                Text("Add")
+            }
+        } message: {
+            Text("One of the selected mantras is already in your mantra list. Add anyway?")
+        }
         .navigationTitle("Mantras Choice")
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
@@ -29,8 +44,7 @@ struct PreloadedMantraListView: View {
                 }
             }
             Button {
-                viewModel.addMantras()
-                isPresented = false
+                viewModel.checkForDuplication($isPresented)
             } label: {
                 Text("Add")
             }

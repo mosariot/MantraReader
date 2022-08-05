60d7e0b68c3091a0b1d830ef3dec6918d91b61d6
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-05 14:00:18 +0300

    Alert for preloading mantras due to no internet connection

--- MantraReader/MantraReaderApp.swift
+++ MantraReader/MantraReaderApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct MantraReaderApp: App {
     @AppStorage("isFirstLaunchSwiftUIVersion") private var isFirstLaunchSwiftUIVersion = true
+    @AppStorage("isPreloadedMantrasDueToNoInternet") private var isPreloadedMantrasDueToNoInternet = false
     @AppStorage("isFreshLaunch") private var isFreshLaunch = true
     @AppStorage("isPresentedOnboarding") private var isPresentedOnboarding = true
     
@@ -22,12 +23,22 @@ struct MantraReaderApp: App {
                 .environmentObject(OrientationInfo())
                 .onAppear {
                     if isFirstLaunchSwiftUIVersion {
+                        isFirstLaunchSwiftUIVersion = false
+                        persistenceController.preloadData(context: persistenceController.container.viewContext)
 //                        let launchPreparer = LaunchPreparer(persistenceController: persistenceController)
 //                        launchPreparer.firstLaunchChecks()
-                        persistenceController.preloadData(context: persistenceController.container.viewContext)
-                        isFirstLaunchSwiftUIVersion = false
                     }
                     isFreshLaunch = true
+                }
+                .alert(
+                    "No Internet Connection",
+                    isPresented: $isPreloadedMantrasDueToNoInternet
+                ) {
+                    Button("OK") {
+                        isPreloadedMantrasDueToNoInternet = false
+                    }
+                } message: {
+                    Text("It seems like there is no internet connection right now. New set of mantras was preloaded. If you were using 'Mantra Reader' previously with enabled iCloud account, your recordings will be added to the list automatically when internet connection will be available (you may need to relaunch the app).")
                 }
                 .sheet(isPresented: $isPresentedOnboarding) {
                     OnboardingView(isPresented: $isPresentedOnboarding)

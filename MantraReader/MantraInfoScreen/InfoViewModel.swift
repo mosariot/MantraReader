c78c5e7e0461d9c247bb10701e2d5452deceed51
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-11 14:58:08 +0300

    InfoViewModel implementation

--- MantraReader/MantraInfoScreen/InfoViewModel.swift
+++ MantraReader/MantraInfoScreen/InfoViewModel.swift
@@ -9,5 +9,32 @@ import SwiftUI
 
 @MainActor
 final class InfoViewModel: ObservableObject {
+    @Published var mantra: Mantra
+    @Published var title: String
+    @Published var text: String
+    @Published var description: String
+    @Published var image: UIImage
     
+    private var viewContext: NSManagedObjectContext
+    
+    init(_ mantra: Mantra, viewContext: NSManagedObjectContext) {
+        self.mantra = mantra
+        self.title = mantra.title ?? ""
+        self.text = mantra.text ?? ""
+        self.description = mantra.details ?? ""
+        self.image = UIImage {
+            if let data = mantra.image, let image = UIImage(data: data) {
+                return image
+            } else {
+                return UIImage(named: Constants.defaultImage)!
+            }
+        }
+        self.viewContext = viewContext
+    }
+    
+    func saveMantra() {
+    }
+    
+    func deleteNewMantra() {
+    }
 }

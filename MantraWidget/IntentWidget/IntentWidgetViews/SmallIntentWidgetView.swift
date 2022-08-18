148931a4d0d4fd91e7b26f20b6fda75149ce7da1
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-18 10:21:17 +0300

    SmallIntentWidgetView tuning

--- MantraWidget/IntentWidget/IntentWidgetViews/SmallIntentWidgetView.swift
+++ MantraWidget/IntentWidget/IntentWidgetViews/SmallIntentWidgetView.swift
@@ -18,23 +18,24 @@ struct SmallIntentWidgetView: View {
              VStack {
                  ZStack {
                      PercentageRing(
-                         ringWidth: 10, percent: Double(selectedMantra.reads)/Double(selectedMantra.goal) * 100,
+                         ringWidth: 10, percent: Double(selectedMantra?.reads ?? 34568)/Double(selectedMantra.goal ?? 100000) * 100,
                          backgroundColor: .red.opacity(0.2),
                          foregroundColors: [
                              Color(red: 0.880, green: 0.000, blue: 0.100),
                              Color(red: 1.000, green: 0.200, blue: 0.540)
                          ]
                      )
-                     Text("\(selectedMantra.reads)")
+                     Text("\(selectedMantra?.reads ?? 34568)")
                         .font(.system(.headline, weight: .bold))
                         .privacySensitive()
                  }
-                 Text(selectedMantra.title)
+                 Text(selectedMantra?.title ?? "Avalokitesvara")
                      .font(.system(.subheadline, weight: .bold))
                      .multilineTextAlignment(.center)
                      .lineLimit(2)
              }
              .redacted(reason: reasons)
          }
+        .widgetURL(URL(string: "\(selectedMantra?.id)"))
     }
 }

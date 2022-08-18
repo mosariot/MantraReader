16d510781e5d47f0a75a0985a0ca1a0b7ce8640d
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-18 10:43:54 +0300

    MediumIntentWidgetView implementation

--- MantraWidget/IntentWidget/IntentWidgetViews/MediumIntentWidgetView.swift
+++ MantraWidget/IntentWidget/IntentWidgetViews/MediumIntentWidgetView.swift
@@ -12,6 +12,37 @@ struct MediumIntentWidgetView: View {
     var selectedMantra: WidgetModel.WidgetMantra?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+             Color(uiColor: UIColor.systemGroupedBackground)
+                 .ignoresSafeArea()
+             HStack {
+                 VStack(alignment: .leading) {
+                     Image(uiImage: (((selectedMantra.image != nil) ?
+                                      UIImage(data: selectedMantra.image!) :
+                                          UIImage(named: Constants.defaultImage)?.resize(to: CGSize(width: Constants.rowHeight, height: Constants.rowHeight)))!))
+                         .resizable()
+                         .frame(width: 41, height: 41, alignment: .center)
+                     Text(selectedMantra?.title ?? "Your mantra")
+                         .font(.system(.subheadline, weight: .bold))
+                         .multilineTextAlignment(.center)
+                         .lineLimit(2)
+                 }
+                 ZStack {
+                     PercentageRing(
+                         ringWidth: 10, percent: Double(selectedMantra?.reads ?? 34568) / Double(selectedMantra.goal ?? 100000) * 100,
+                         backgroundColor: .red.opacity(0.2),
+                         foregroundColors: [
+                             Color(red: 0.880, green: 0.000, blue: 0.100),
+                             Color(red: 1.000, green: 0.200, blue: 0.540)
+                         ]
+                     )
+                     Text("\(selectedMantra?.reads ?? Int32(34568))")
+                        .font(.system(.headline, weight: .bold))
+                        .privacySensitive()
+                 }
+             }
+             .redacted(reason: reasons)
+         }
+        .widgetURL(URL(string: "\(selectedMantra?.id)"))
     }
 }

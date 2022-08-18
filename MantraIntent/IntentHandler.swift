0d4e5214f0f26e09e3a4a8eab0111cf08d11e0dd
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-18 09:34:22 +0300

    IntentHandler refactoring

--- MantraIntent/IntentHandler.swift
+++ MantraIntent/IntentHandler.swift
@@ -2,7 +2,7 @@
 //  IntentHandler.swift
 //  MantraIntent
 //
-//  Created by Александр Воробьев on 17.08.2022.
+//  Created by Alex Vorobiev on 17.08.2022.
 //
 
 import Intents
@@ -15,12 +15,8 @@ class IntentHandler: INExtension, SelectMantraIntentHandling {
     func provideMantraOptionsCollection(for intent: SelectMantraIntent, with completion: @escaping (INObjectCollection<WidgetIntentMantra>?, Error?) -> Void) {
         guard let widgetItem = try? JSONDecoder().decode(WidgetModel.self, from: widgetItemData) else { return }
         let mantras: [WidgetIntentMantra] = widgetItem.mantras
-            .sorted { $0.title < $1.title }
-            .map { mantra in
-                let widgetIntentMantra = WidgetIntentMantra(identifier: mantra.id.uuidString, display: mantra.title)
-                return widgetIntentMantra
-            }
-        
+            .sorted(using: KeyPathComparator(\.title, order: .forward))
+            .map { WidgetIntentMantra(identifier: $0.id.uuidString, display: $0.title) }
         let collection = INObjectCollection(items: mantras)
         completion(collection, nil)
     }

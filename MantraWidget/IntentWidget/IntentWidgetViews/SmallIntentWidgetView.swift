a5ccd019848c9d3bdc8fa441895310d7b715626a
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-18 10:12:38 +0300

    SmallIntentWidgetView implementation

--- MantraWidget/IntentWidget/IntentWidgetViews/SmallIntentWidgetView.swift
+++ MantraWidget/IntentWidget/IntentWidgetViews/SmallIntentWidgetView.swift
@@ -12,6 +12,29 @@ struct SmallIntentWidgetView: View {
     var selectedMantra: WidgetModel.WidgetMantra?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+         ZStack {
+             Color(uiColor: UIColor.systemGroupedBackground)
+                 .ignoresSafeArea()
+             VStack {
+                 ZStack {
+                     PercentageRing(
+                         ringWidth: 10, percent: Double(selectedMantra.reads)/Double(selectedMantra.goal) * 100,
+                         backgroundColor: .red.opacity(0.2),
+                         foregroundColors: [
+                             Color(red: 0.880, green: 0.000, blue: 0.100),
+                             Color(red: 1.000, green: 0.200, blue: 0.540)
+                         ]
+                     )
+                     Text("\(selectedMantra.reads)")
+                        .font(.system(.headline, weight: .bold))
+                        .privacySensitive()
+                 }
+                 Text(selectedMantra.title)
+                     .font(.system(.subheadline, weight: .bold))
+                     .multilineTextAlignment(.center)
+                     .lineLimit(2)
+             }
+             .redacted(reason: reasons)
+         }
     }
 }

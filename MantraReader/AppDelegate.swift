a68b03dc4c188627212d94ccf163cedf16294c2d
mosariot <mosariot@gmail.com>
2022-08-21 17:19:11 +0300

    Intent localization. Preparations for PHPicker and Home Screen Quick Actions

---
+++ MantraReader/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  MantraReader
+//
+//  Created by Александр Воробьев on 21.08.2022.
+//
+
+import Foundation
---
+++ MantraReader/MantraInfoScreen/ImagePickerView.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePickerView.swift
+//  MantraReader
+//
+//  Created by Александр Воробьев on 21.08.2022.
+//
+
+import Foundation

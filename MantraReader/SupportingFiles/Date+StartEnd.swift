459fde8c4303b7f73c0142447b6fa737ee102ece
mosariot <56510738+mosariot@users.noreply.github.com>
2022-08-31 13:10:14 +0300

    Date+StartEnd polishing

--- MantraReader/SupportingFiles/Date+StartEnd.swift
+++ MantraReader/SupportingFiles/Date+StartEnd.swift
@@ -13,28 +13,26 @@ extension Date {
     }
 
     var startOfMonth: Date {
-        let calendar = Calendar(identifier: .gregorian)
         let components = calendar.dateComponents([.year, .month], from: self)
-        return  calendar.date(from: components)!
+        return Calendar(identifier: .gregorian).date(from: components)!
     }
 
     var endOfDay: Date {
         var components = DateComponents()
         components.day = 1
-        components.second = -1
+        components.nanosecond = -1
         return Calendar.current.date(byAdding: components, to: startOfDay)!
     }
 
     var endOfMonth: Date {
         var components = DateComponents()
         components.month = 1
-        components.second = -1
+        components.nanosecond = -1
         return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfMonth)!
     }
 
     func isMonday() -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
-        let components = calendar.dateComponents([.weekday], from: self)
+        let components = Calendar(identifier: .gregorian).dateComponents([.weekday], from: self)
         return components.weekday == 2
     }
 }

974c873093de6b5301a3372744bc77e1d533a3be
mosariot <56510738+mosariot@users.noreply.github.com>
2022-09-01 14:59:08 +0300

    Date+Stridable

--- MantraReader/SupportingFiles/Date+StartEnd.swift
+++ MantraReader/SupportingFiles/Date+StartEnd.swift
@@ -30,9 +30,31 @@ extension Date {
         components.nanosecond = -1
         return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfMonth)!
     }
+    
+    var startOfMonth: Date {
+        let components = Calendar(identifier: .gregorian).dateComponents([.year], from: self)
+        return Calendar(identifier: .gregorian).date(from: components)!
+    }
+    
+    var endOfYear: Date {
+        var components = DateComponents()
+        components.year = 1
+        components.nanosecond = -1
+        return Calendar(identifier: .gregorian).date(byAdding: components, to: startOfYear)!
+    }
 
     func isMonday() -> Bool {
         let components = Calendar(identifier: .gregorian).dateComponents([.weekday], from: self)
         return components.weekday == 2
+    }
+}
+
+extension Date: Strideable {
+    public func distance(to other: Date) -> TimeInterval {
+        return other.timeIntervalSinceReferenceDate - self.timeIntervalSinceReferenceDate
+    }
+
+    public func advanced(by n: TimeInterval) -> Date {
+        return self + n
     }
 }

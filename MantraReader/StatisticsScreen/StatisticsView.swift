9d395b057012223201c76a15c2d40bd9414f42ca
mosariot <56510738+mosariot@users.noreply.github.com>
2022-09-01 09:45:06 +0300

    Preparations for data passing StatisticsView

--- MantraReader/StatisticsScreen/StatisticsView.swift
+++ MantraReader/StatisticsScreen/StatisticsView.swift
@@ -10,20 +10,38 @@ import SwiftUI
 struct StatisticsView: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject var viewModel: StatisticsViewModel
-    @State private var monthHeader = String(localized: "Month")
-    @State private var yearHeader = String(localized: "Year")
+    @State private var selectedMonth: Int = 0
+    @State private var selectedYear: Int = 0
+    private var currentMonth: Int { Calendar(identifier: .gregorian).dateComponents([.month], from: Date()).month! }
+    private var currentYear: Int { Calendar(identifier: .gregorian).dateComponents([.year], from: Date()).year! }
+    private var monthHeader: String {
+            switch selectedMonth {
+            case 0: monthHeader = String(localized: "Month")
+            case 1...currentMonth: monthHeader = date(year: currentYear, month: newValue).formatted(.dateTime.month(.wide))
+            default: monthHeader = String(localized: "Month")
+        }
+    }
+    private var yearHeader: String {
+        switch selectedYear {
+            case 0: yearHeader = String(localized: "Year")
+            case 2022...currentYear: yearHeader = date(year: newValue).formatted(.dateTime.year())
+            default: yearHeader = String(localized: "Year")
+        }
+    }
     
     var body: some View {
         NavigationStack {
             List {
                 Section("Week") {
-                    WeekStatisticsView()
+                    WeekStatisticsView(data: viewModel.weekData)
                 }
                 Section(monthHeader) {
-                    MonthStatisticsView(monthHeader: $monthHeader)
+                    MonthStatisticsView(data: viewModel.monthData, selectedMonth: $selectedMonth)
+                        .animation(.easeInOut, value: selectedMonth)
                 }
                 Section(yearHeader) {
-                    YearStatisticsView(yearHeader: $yearHeader)
+                    YearStatisticsView(data: viewModel.yearData, selectedYear: $selectedYear)
+                        .animation(.easeInOut, value: selectedYear)
                 }
             }
             .navigationTitle(viewModel.navigationTitle)
@@ -38,6 +56,12 @@ struct StatisticsView: View {
                             .foregroundColor(.gray.opacity(0.8))
                     }
                 }
+            }
+            .onChange(of: selectedMonth) { newValue in
+                print("to handle data providing in viewmodel")
+            }
+            .onChange(of: selectedYear) { newValue in
+                print("to handle data providing in viewmodel")
             }
         }
     }

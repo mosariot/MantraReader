d729c6778d0c6de19a0643f9c19eee8e5449e948
mosariot <56510738+mosariot@users.noreply.github.com>
2022-09-07 12:56:25 +0300

    YearStatisticsView buttons spacing

--- MantraReader/StatisticsScreen/YearStatisticsView.swift
+++ MantraReader/StatisticsScreen/YearStatisticsView.swift
@@ -127,7 +127,7 @@ struct YearStatisticsView: View {
                 .tint(.red.opacity(0.8))
                 .disabled(selectedYear == 2022 || (selectedYear == 0 && currentYear == 2022))
                 Spacer()
-                    .frame(minWidth: 0, maxWidth: !(horizontalSizeClass == .compact && verticalSizeClass == .regular) ? 40 : nil)
+                    .frame(minWidth: 0, maxWidth: !(horizontalSizeClass == .compact && verticalSizeClass == .regular) ? 60 : nil)
                 Picker("", selection: $selectedYear) {
                     Text("Last 12 Months").tag(0)
                     ForEach((2022...currentYear).reversed(), id: \.self) {
@@ -137,7 +137,7 @@ struct YearStatisticsView: View {
                 .layoutPriority(1)
                 .labelsHidden()
                 Spacer()
-                    .frame(minWidth: 0, maxWidth: !(horizontalSizeClass == .compact && verticalSizeClass == .regular) ? 40 : nil)
+                    .frame(minWidth: 0, maxWidth: !(horizontalSizeClass == .compact && verticalSizeClass == .regular) ? 60 : nil)
                 Button {
                     if selectedYear == 0 {
                         selectedYear = currentYear

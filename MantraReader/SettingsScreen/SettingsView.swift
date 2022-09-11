330fbbc745789749ed1402dd8e26087afd309eda
mosariot <mosariot@gmail.com>
2022-09-11 20:03:39 +0300

    SettingsView start

---
+++ MantraReader/SettingsScreen/SettingsView.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsView.swift
+//  MantraReader
+//
+//  Created by Alex Vorobiev on 11.09.2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}

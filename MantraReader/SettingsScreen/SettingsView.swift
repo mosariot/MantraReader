c47eded4a730b159db9d44deba15f4d6db0d974f
mosariot <56510738+mosariot@users.noreply.github.com>
2022-09-12 16:30:36 +0300

    RingColors detailed implementation

--- MantraReader/SettingsScreen/SettingsView.swift
+++ MantraReader/SettingsScreen/SettingsView.swift
@@ -20,6 +20,50 @@ enum RingColor: String, Hashable {
     case green
     case blue
     case dynamic
+    
+    var colors: [Color] {
+        switch self {
+        case .red: return [.progressRedStart, .progressRedEnd]
+        case .green: return [.progressGreenStart, .progressGreenEnd]
+        case .blue: return [.progressBlueStart, .progressBlueEnd]
+        default: return [.red]
+        }
+    }
+}
+
+extension Color {
+    static let progressRedStart = Color("progressStart")
+    static let progressRedEnd = Color("progressEnd")
+    static let progressBlueStart = Color("progressBlueStart")
+    static let progressBlueEnd = Color("progressBlueEnd")
+    static let progressGreenStart = Color("progressGreenStart")
+    static let progressGreenEnd = Color("progressGreenEnd")
+}
+
+struct CornerRadiusShape: Shape {
+    var radius = CGFloat.infinity
+    var corners = UIRectCorner.allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+struct CornerRadiusStyle: ViewModifier {
+    var radius: CGFloat
+    var corners: UIRectCorner
+
+    func body(content: Content) -> some View {
+        content
+            .clipShape(CornerRadiusShape(radius: radius, corners: corners))
+    }
+}
+
+extension View {
+    func cornerRadius(radius: CGFloat, corners: UIRectCorner) -> some View {
+        ModifiedContent(content: self, modifier: CornerRadiusStyle(radius: radius, corners: corners))
+    }
 }
 
 struct SettingsView: View {
@@ -40,25 +84,33 @@ struct SettingsView: View {
                     RoundedRectangle(cornerRadius: 10, style: .continuous)
                         .fill(
                             LinearGradient(
-                                gradient: Gradient(colors: [Color(Constants.progressStartColor), Color(Constants.progressEndColor)],
+                                gradient: Gradient(colors: RingColor.red.colors],
                                 startPoint: .leading,
                                 endPoint: .trailing
                             )
                         )
                         .frame(maxWidth: 200)
                         .tag(RingColor.red)
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    RoundedRectangle(cornerRadius: 10)
                         .fill(.green)
                         .frame(maxWidth: 200)
                         .tag(RingColor.green)
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    RoundedRectangle(cornerRadius: 10)
                         .fill(.blue)
                         .frame(maxWidth: 200)
                         .tag(RingColor.blue)
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(.gray)
-                        .frame(maxWidth: 200)
-                        .tag(RingColor.dynamic)
+                    HStack {
+                        Rectangle()
+                            .fill(.blue)
+                            .cornerRadius(radius: 10.0, corners: [.topLeft, .bottomLeft])
+                        Rectangle()
+                            .fill(.green)
+                        Rectangle()
+                            .fill(.red)
+                            .cornerRadius(radius: 10.0, corners: [.topRight, .bottomRight])
+                    }
+                    .frame(maxWidth: 200)
+                    .tag(RingColor.dynamic)
                 }
                 Picker("Appearance", selection: $colorScheme) {
                     ForEach(ColorScheme.allCases) { scheme in

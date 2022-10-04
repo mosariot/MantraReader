f26f820ed48ac8428a350e0f0999be2ea086dae3
mosariot <56510738+mosariot@users.noreply.github.com>
2022-10-04 08:52:57 +0300

    Line color and widgetAccentable for rectangular intent widget

--- MantraWidget/IntentWidget/IntentWidgetViews/AccessoryRectangularIntentWidgetVIew.swift
+++ MantraWidget/IntentWidget/IntentWidgetViews/AccessoryRectangularIntentWidgetVIew.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct AccessoryRectangularIntentWidgetVIew: View {
     @Environment(\.redactionReasons) private var reasons
+    @Environment(\.widgetRenderingMode) private var widgetRenderingMode
+    @EnvironmentObject private var settings: Settings
     var selectedMantra: WidgetModel.WidgetMantra?
     var firstMantra: WidgetModel.WidgetMantra?
+    
+    private var lineColor: Color {
+        switch settings.ringColor {
+            case .dynamic:
+            let progress = Double((selectedMantra?.reads ?? firstMantra?.reads) ?? 0) / Double((selectedMantra?.goal ?? firstMantra?.goal) ?? 100000)
+            if progress < 0.5 {
+                return .progressGreenStart
+            } else if progress >= 0.5 && progress < 1.0 {
+                return .progressYellowStart
+            } else if progress >= 1.0 {
+                return .progressRedStart
+            } else {
+                return .accentColor
+            }
+            case .red: return .progressRedStart
+            case .yellow: return .progressYellowStart
+            case .green: return .progressGreenStart
+        }
+    }
     
     var body: some View {
         Gauge(
@@ -18,11 +39,13 @@ struct AccessoryRectangularIntentWidgetVIew: View {
             in: 0...Double((selectedMantra?.goal ?? firstMantra?.goal) ?? 100000)
         ) {
             Text((selectedMantra?.title ?? firstMantra?.title) ?? "Your mantra")
+                .widgetAccentable()
         } currentValueLabel: {
             Text("\((selectedMantra?.reads ?? firstMantra?.reads) ?? 0)")
                 .privacySensitive()
         }
         .gaugeStyle(.accessoryLinearCapacity)
+        .tint(widgetRenderingMode == .fullColor ? lineColor : nil)
         .redacted(reason: reasons)
         .widgetURL(URL(string: (selectedMantra?.id.uuidString ?? firstMantra?.id.uuidString) ?? ""))
     }

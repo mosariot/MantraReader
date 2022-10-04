29945d1bef0de00fe9718e4e9a9361fd10d200e8
mosariot <56510738+mosariot@users.noreply.github.com>
2022-10-04 08:49:41 +0300

    Ring color for circular intent widget

--- MantraWidget/IntentWidget/IntentWidgetViews/AccessoryCircularIntentWidgetView.swift
+++ MantraWidget/IntentWidget/IntentWidgetViews/AccessoryCircularIntentWidgetView.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 struct AccessoryCircularIntentWidgetView: View {
     @Environment(\.redactionReasons) private var reasons
+    @Environment(\.widgetRenderingMode) private var widgetRenderingMode
+    @EnvironmentObject private var settings: Settings
     var selectedMantra: WidgetModel.WidgetMantra?
     var firstMantra: WidgetModel.WidgetMantra?
     let formatter = KMBFormatter()
+    
+    private var ringColor: Color {
+        switch settings.ringColor {
+            case .dynamic:
+            let progress = Double((selectedMantra?.reads ?? firstMantra?.reads) ?? 0) / Double((selectedMantra?.goal ?? firstMantra?.goal) ?? 100000)
+            if progress < 0.5 {
+                return .progressGreenStart
+            } else if progress >= 0.5 && progress < 1.0 {
+                return .progressYellowStart
+            } else if progress >= 1.0 {
+                return .progressRedStart
+            } else {
+                return .accentColor
+            }
+            case .red: return .progressRedStart
+            case .yellow: return .progressYellowStart
+            case .green: return .progressGreenStart
+        }
+    }
     
     var body: some View {
         Gauge(
@@ -24,6 +45,7 @@ struct AccessoryCircularIntentWidgetView: View {
                 .privacySensitive()
         }
         .gaugeStyle(.accessoryCircularCapacity)
+        .tint(widgetRenderingMode == .fullColor ? ringColor : nil)
         .redacted(reason: reasons)
         .widgetURL(URL(string: (selectedMantra?.id.uuidString ?? firstMantra?.id.uuidString) ?? ""))
     }

accbcb249451fe759bde04d03ce428035633bac8
mosariot <56510738+mosariot@users.noreply.github.com>
2022-10-04 10:12:37 +0300

    accessoryCorner static widget

--- MantraWidget/StaticWidget/StaticWidgetEntryView.swift
+++ MantraWidget/StaticWidget/StaticWidgetEntryView.swift
@@ -35,6 +35,15 @@ struct StaticWidgetEntryView: View {
         case .systemLarge:
             LargeStaticWidgetView(widgetModel: entry.widgetModel)
                 .environment(\.colorScheme, preferredColorScheme ?? systemColorScheme)
+#elseif os(watchOS)
+        case .accessoryCorner:
+            VStack {
+                Image(systemName: "book")
+                    .imageScale(.large)
+                    .widgetAccentable
+                Text("\(entry.widgetModel.mantras.map { $0.reads }.reduce(0,+))")
+                    .privacySensitive()
+            }
 #endif
         case .accessoryInline:
             Text("Mantras: \(entry.widgetModel.mantras.map { $0.reads }.reduce(0,+))")
